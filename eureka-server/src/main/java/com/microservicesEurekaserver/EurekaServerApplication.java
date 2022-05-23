007491c90756aa705e2d9e78b2e8c9336c3d50d5
unknown <marcio.greco@outlook.com>
2022-05-23 18:24:42 -0300

    Configuring eureka server

--- eureka-server/src/main/java/com/microservicesEurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/microservicesEurekaserver/EurekaServerApplication.java
@@ -2,7 +2,8 @@ package com.microservicesEurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
